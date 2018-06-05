b40f80101ea75addae09c6d7c95120199fbdb0a9
Marek Anderson <marek.anderson@stud.fh-luebeck.de>
2018-06-05 23:10:47 +0200

    right order according to matrix; grep read-write from logs

--- run-fio.sh
+++ run-fio.sh
@@ -16,33 +16,32 @@ echo "per_job_logs=0" >> ${FIO_JOB}
 echo "write_bw_log=bw-${START_DATETIME}" >> ${FIO_JOB}
 mkdir ${PLOT_OUTPUT_DIR}
 
-#sudo -v && for sched in "${SCHEDULERS[@]}"; do
-#  echo Try setting ${sched} as I/O scheduler
-#  echo "${sched}" | sudo tee /sys/block/sda/queue/scheduler > /dev/null
-#  echo Check current scheduler:
-#  sed -e 's/.*\[\(.*\)\].*/\1/' /sys/block/sda/queue/scheduler
-#  for testrun in $(seq 0 1); do
-#    echo Test:"${testrun}" fs:"${FS_TYPE}" scheduler:"${sched}"
-#    sudo fio ${FIO_JOB} | grep "bw=" | tee -a ${PLOT_OUTPUT_DIR}/fio-average-${START_DATETIME}.log
-#  done
-#done
-
-#sudo -v && for fs in "${FS_TYPE[@]}"; do
 for fs in "${FS_TYPE[@]}"; do
   if [[ ${fs} == "xfs" ]]; then
     cd /mnt/
   fi
   for sched in "${SCHEDULERS[@]}"; do
-#    echo Try setting ${sched} as I/O scheduler
     echo "${sched}" | sudo tee /sys/block/sda/queue/scheduler > /dev/null
     echo -n "Current scheduler was just changed to : "
     sed -e 's/.*\[\(.*\)\].*/\1/' /sys/block/sda/queue/scheduler
-    for testrun in $(seq 0 1); do
+    for testrun in $(seq 1 2); do
       echo Test:"${testrun}" fs:"${fs}" scheduler:"${sched}"
       sudo fio ${HOME}/${FIO_JOB} | grep "bw=" | tee -a ${PLOT_OUTPUT_DIR}/fio-average-${START_DATETIME}-${fs}-${sched}.log
     done
   done
 done
+
+# prepare data for plotting
+cd ${PLOT_OUTPUT_DIR}
+for logfile in $(ls -1); do
+  readlog=$(basename ${logfile} .log)-read.log
+  writelog=$(basename ${logfile} .log)-write.log
+  #echo $readname
+  #grep read ${logfile} | cut -f3 -d'=' | cut -f1 -d'K' > ${readlog}
+  grep read ${logfile} | sed -e 's/.*\(bw=\)\(.*\)KB.*/\2/' > ${readlog}
+  grep write ${logfile} | sed -e 's/.*\(bw=\)\(.*\)KB.*/\2/' > ${writelog}
+done
+
 
 #mv bw-${START_DATETIME}* ${PLOT_OUTPUT_DIR}
 #cd ${PLOT_OUTPUT_DIR};
