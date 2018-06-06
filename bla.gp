1c588af794c2ca115a3d0126ad1860ccc30a129a
Marek Anderson <marek.anderson@stud.fh-luebeck.de>
2018-06-06 15:00:26 +0200

    cleaner run-fio script; gnuplot script; examples

--- bla.gp
+++ bla.gp
@@ -1,7 +1,11 @@
-plot "fio-average-2018-06-05_1901-ext4-deadline-read.log" using 1:2 notitle
+plot "fio-average-2018-06-06_1234-ext4-deadline-read.log" using 1:2 with linespoints title 'ext4-deadline-read', \
+"fio-average-2018-06-06_1234-ext4-deadline-write.log" using 1:2 with linespoints title 'ext4-deadline-write', \
+"fio-average-2018-06-06_1234-ext4-noop-read.log" using 1:2 with linespoints title 'ext4-noop-read', \
+"fio-average-2018-06-06_1234-ext4-noop-write.log" using 1:2 with linespoints title 'ext4-noop-write
 set title 'Test'
+set key outside left box
 set ylabel 'Bandwidth in KB/sec'
-set xrange [0:3]
 set pointsize 1.5
 replot
 pause -1 "Hit any key to continue"
+#fio-average-2018-06-06_1234-ext4-noop-read.log fio-average-2018-06-06_1234-ext4-noop-write.log
--- run-fio.sh
+++ run-fio.sh
@@ -9,6 +9,7 @@ declare START_DATETIME=$(date +%F_%H%M)
 declare PLOT_OUTPUT_DIR=${HOME}/gnuplot-${START_DATETIME}
 declare -a FS_TYPE=("ext4" "xfs")
 declare -a SCHEDULERS=("noop" "deadline")
+declare TIMES_TO_RUN=1
 
 cd ${HOME}
 cp /usr/share/doc/fio/examples/${FIO_JOB} .
@@ -22,22 +23,22 @@ for fs in "${FS_TYPE[@]}"; do
   fi
   for sched in "${SCHEDULERS[@]}"; do
     echo "${sched}" | sudo tee /sys/block/sda/queue/scheduler > /dev/null
-    echo -n "Current scheduler was just changed to : "
-    sed -e 's/.*\[\(.*\)\].*/\1/' /sys/block/sda/queue/scheduler
-    for testrun in $(seq 1 2); do
-      echo Test:"${testrun}" fs:"${fs}" scheduler:"${sched}"
-      sudo fio ${HOME}/${FIO_JOB} | grep "bw=" | tee -a ${PLOT_OUTPUT_DIR}/fio-average-${START_DATETIME}-${fs}-${sched}.log
+    #echo -n "Current scheduler was just changed to : "
+    #sed -e 's/.*\[\(.*\)\].*/\1/' /sys/block/sda/queue/scheduler
+    for testrun in $(seq 1 ${TIMES_TO_RUN}); do
+      echo -ne "\r\e[KMeasurement ${testrun} of ${TIMES_TO_RUN} fs:${fs} scheduler:${sched}"
+      sudo fio ${HOME}/${FIO_JOB} | grep "bw=" >> ${PLOT_OUTPUT_DIR}/fio-average-${START_DATETIME}-${fs}-${sched}.log
     done
   done
 done
+
+echo ""
 
 # prepare data for plotting
 cd ${PLOT_OUTPUT_DIR}
 for logfile in $(ls -1); do
   readlog=$(basename ${logfile} .log)-read.log
   writelog=$(basename ${logfile} .log)-write.log
-  #echo $readname
-  #grep read ${logfile} | cut -f3 -d'=' | cut -f1 -d'K' > ${readlog}
   grep read ${logfile} | sed -e 's/.*\(bw=\)\(.*\)KB.*/\2/' > ${readlog}
   grep write ${logfile} | sed -e 's/.*\(bw=\)\(.*\)KB.*/\2/' > ${writelog}
 done
